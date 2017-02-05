9fd160d5a88bdb0fb4c832e9171a794acc3f7f21
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-06 00:32:02 +0300

    no message

---
+++ view/frontend/web/main.js
@@ -0,0 +1,30 @@
+// 2017-02-06
+define([
+	'Df_Payment/card'
+], function(parent) {'use strict'; return parent.extend({
+	/**
+	 * 2017-02-06
+	 * The bank card network codes: https://mage2.pro/t/2647
+	 * @returns {String[]}
+	 */
+	getCardTypes: function() {return ['VI', 'MC', 'AE', 'JCB', 'DI', 'DN'];},
+	/**
+	 * 2017-02-06
+	 * @returns {Object}
+	*/
+	initialize: function() {
+		this._super();
+		return this;
+	},
+	/**
+	 * 2017-02-06
+	 * @override
+	 * @see https://github.com/magento/magento2/blob/2.1.0/app/code/Magento/Checkout/view/frontend/web/js/view/payment/default.js#L127-L159
+	 * @used-by https://github.com/magento/magento2/blob/2.1.0/lib/web/knockoutjs/knockout.js#L3863
+	 * @param {this} _this
+	*/
+	placeOrder: function(_this) {
+		if (this.validate()) {
+		}
+	}
+});});
