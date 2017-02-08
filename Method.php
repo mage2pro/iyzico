b2eedbed80eabb844b85e254539dbe76b83ef0a0
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-08 22:14:45 +0300

    no message

--- Method.php
+++ Method.php
@@ -5,6 +5,20 @@ use Magento\Sales\Model\Order\Payment\Transaction as T;
 /** @method Settings s() */
 final class Method extends \Df\StripeClone\Method {
 	/**
+	 * 2017-02-08
+	 * @override
+	 * Результат — в рублях, не в копейках.
+	 * I did not find such information on the iyzico websites.
+	 * «Does iyzico have minimum and maximum amount limitations on a single payment?»
+	 * https://mage2.pro/t/2684
+	 * https://iyzico.zendesk.com/hc/tr/requests/77724
+	 * @see \Df\Payment\Method::amountLimits()
+	 * @used-by \Df\Payment\Method::isAvailable()
+	 * @return null
+	 */
+	protected function amountLimits() {return null;}
+
+	/**
 	 * 2017-02-05
 	 * Информация о банковской карте.
 	 * @override
