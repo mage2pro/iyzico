9c722335d8fad1db680423d0e25a772509970125
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-11 01:02:23 +0300

    no message

--- Method.php
+++ Method.php
@@ -23,17 +23,6 @@ final class Method extends \Df\StripeClone\Method {
 
 	/**
 	 * 2017-02-05
-	 * Информация о банковской карте.
-	 * @override
-	 * @see \Df\StripeClone\Method::apiCardInfo()
-	 * @used-by \Df\StripeClone\Method::chargeNew()
-	 * @param object $charge
-	 * @return array(string => string)
-	 */
-	protected function apiCardInfo($charge) {return [];}
-
-	/**
-	 * 2017-02-05
 	 * @override
 	 * @see \Df\StripeClone\Method::responseToArray()
 	 * @used-by \Df\StripeClone\Method::transInfo()
