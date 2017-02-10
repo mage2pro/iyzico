1818bcc84b7b41a2798cc14ff003abd33cade2de
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-10 19:45:25 +0300

    no message

--- Method.php
+++ Method.php
@@ -7,7 +7,7 @@ final class Method extends \Df\StripeClone\Method {
 	/**
 	 * 2017-02-08
 	 * @override
-	 * Результат — в рублях, не в копейках.
+	 * The result should be in the basic monetary unit (like dollars), not in fractions (like cents).
 	 * I did not find such information on the iyzico websites.
 	 * «Does iyzico have minimum and maximum amount limitations on a single payment?»
 	 * https://mage2.pro/t/2684
