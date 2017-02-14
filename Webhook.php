307a546b36311f3add4889b9ee11b46136747d0d
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-14 10:48:59 +0300

    no message

--- Webhook.php
+++ Webhook.php
@@ -5,15 +5,6 @@ abstract class Webhook extends \Df\StripeClone\Webhook {
 	/**
 	 * 2017-02-14
 	 * @override
-	 * @see \Df\Payment\Webhook::parentIdRawKey()
-	 * @used-by \Df\Payment\Webhook::parentIdRaw()
-	 * @return string
-	 */
-	protected function parentIdRawKey() {return '';}
-
-	/**
-	 * 2017-02-14
-	 * @override
 	 * @see \Df\StripeClone\Webhook::roPath()
 	 * @used-by \Df\StripeClone\Webhook::ro()
 	 * @return string
