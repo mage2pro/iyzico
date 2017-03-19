6e70eef2124f0e56a0dcc29511d515083446bda9
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-03-19 12:29:39 +0300

    0.2.1

---
+++ W/Event.php
@@ -0,0 +1,14 @@
+<?php
+namespace Dfe\Iyzico\W;
+// 2017-03-15
+final class Event extends \Df\StripeClone\W\Event {
+	/**
+	 * 2017-02-14
+	 * @override
+	 * @see \Df\StripeClone\W\Event::roPath()
+	 * @used-by \Df\StripeClone\W\Event::k_pid()
+	 * @used-by \Df\StripeClone\W\Event::ro()
+	 * @return string
+	 */
+	protected function roPath() {return '';}
+}
